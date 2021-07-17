c165e841ce8044e2d240d702c17a3d70d10205ae
Arno <arno@reed.be>
2021-07-17 11:22:18 +0200

    Remove draco loader

--- src/js/utils/model-loader-gltf.js
+++ src/js/utils/model-loader-gltf.js
@@ -1,13 +1,13 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+// import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 const loader = new GLTFLoader()
 
 // Optional
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('./node_modules/three/examples/js/libs/draco/')
-dracoLoader.preload()
-loader.setDRACOLoader(dracoLoader)
+// const dracoLoader = new DRACOLoader()
+// dracoLoader.setDecoderPath('./node_modules/three/examples/js/libs/draco/')
+// dracoLoader.preload()
+// loader.setDRACOLoader(dracoLoader)
 
 export default (url, id) => new Promise((resolve, reject) => {
     loader.load(url, (gltf) => {
